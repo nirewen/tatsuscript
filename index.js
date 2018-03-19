c018a96a988c353bf168a0dffba1be2638138aa7
Eduardo Londero <londero.edu@gmail.com>
2018-03-18 21:08:43 -0300

    Remove unecessary lambda

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const Interpreter = require('./src/Interpreter');
 const Parser      = require('./src/Parser');
 const Lexer       = require('./src/Lexer');
 
-module.exports = (() => class TatsuScript {
+module.exports = class TatsuScript {
 	static run(message) {
 		return new Interpreter(message).interpret(Parser.parse(Lexer.lex(message.content)));
 	}
-})();
+};
