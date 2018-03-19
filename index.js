fcc9bc1bf12520cd0ab89153435b06ff63b12601
Eduardo Londero <londero.edu@gmail.com>
2018-03-18 21:25:09 -0300

    Fix indentation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Parser      = require('./src/Parser');
 const Lexer       = require('./src/Lexer');
 
 module.exports = class TatsuScript {
-	static run(message) {
-		return new Interpreter(message).interpret(Parser.parse(Lexer.lex(message.content)));
-	}
+    static run(message) {
+        return new Interpreter(message).interpret(Parser.parse(Lexer.lex(message.content)));
+    }
 };
